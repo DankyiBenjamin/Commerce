3d9485c430b268c5fa82970d753f841c91b9ee6e
Xeon <xeon@Dankyis-MacBook-Pro.local>
2025-01-27 15:50:34 -0500

    model setup

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,23 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class AuctionList(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    starting_bid = models.IntegerField()
+    current_bid = models.IntegerField()
+    image_url = models.CharField()
+    
+
+
+class Bids(models.Model):
+    pass
+
+
+class Comments(models.Model):
+    pass
+
+
+class Watchlist(models.Model):
+    pass
